a4dd6adcb6dcc10463fd294df0f636e34b0cf91e
SK Tech <support@mysktech.com>
2026-09-05 18:58:46 +0530

    Add worker/src/selectors.js

---
+++ worker/src/selectors.js
@@ -0,0 +1,11 @@
+// Keep selectors isolated because job-site UIs change frequently.
+export const selectors = {
+  loginIndicator: 'a[href*="logout"], .nI-gNb-drawer__icon',
+  jobCard: '.srp-jobtuple-wrapper, .jobTuple',
+  jobTitle: 'a.title, .title',
+  jobDescription: '.job-desc, .job-description',
+  location: '.locWdth, .location',
+  experience: '.expwdth, .experience',
+  applyButton: 'button:has-text("Apply"), a:has-text("Apply")',
+  alreadyApplied: 'text=/already applied/i'
+};
